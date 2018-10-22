a0d3805c8b00efdfc65b526457ad5e8ab87520b7
Kenneth Lewenhagen <lewenhagen@live.com>
2018-10-22 23:03:38 +0200

    added option conrol

--- main.bash
+++ main.bash
@@ -7,18 +7,31 @@ declare -r SLEEPER=2
 declare -r GAME=$1
 declare -r NUM_ROWS=10
 declare -r NUM_COLUMNS=20
-declare -r RAND_VAL=$2
+# declare -r RAND_VAL=0
 
 let currtick=1
-
 
 . random_matrix.bash
 . functions.bash
 
-if [ "$1" = "random" ]
-then
-    initMatrixRandom $RAND_VAL
-else
-    initMatrix
-fi
-tick
+while (( $# ))
+do
+    case "$1" in
+
+        --random | -r)
+            shift
+            declare -r RAND_VAL=$1
+            initMatrixRandom $RAND_VAL
+            tick
+            exit 0
+        ;;
+
+        # --version | -v)
+        #     version
+        #     exit 0
+        # ;;
+
+# more code...
+
+    esac
+done
